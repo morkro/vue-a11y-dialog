a16576420e28989ccbc38a24580cd281f8de830b
Moritz Kröger <write@morkro.de>
2018-06-11 10:57:16 +0200

    :tada: (init) Project setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# Vue A11yDialog
---
+++ index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div :id="id" :class="classNames.base" ref="rootElement">
+    <div
+      tabIndex="-1"
+      :class="classNames.overlay"
+      @click="close"
+    />
+
+    <dialog
+      :class="classNames.element"
+      :aria-labelledby="titleId"
+    >
+      <div role="document" :class="classNames.document">
+        <button
+          type="button"
+          :aria-label="closeButtonLabel"
+          @click="close"
+          :class="classNames.closeButton"
+        >
+          <slot name="closeButtonContent">{{'\u00D7'}}</slot>
+        </button>
+
+        <h1 :id="fullTitleId" :class="classNames.title">
+          <slot name="title" />
+        </h1>
+
+        <slot />
+      </div>
+    </dialog>
+  </div>
+</template>
+
+<script>
+  import A11yDialog from 'a11y-dialog'
+
+  export default {
+    name: 'vue-a11y-dialog',
+    props: {
+      id: { type: String, required: true },
+      appRoot: { type: [String, Array], required: true },
+      classNames: { type: Object, default: () => ({}) },
+      titleId: { type: String },
+      closeButtonLabel: { type: String, default: 'Close this dialog window' }
+    },
+    computed: {
+      fullTitleId() {
+        return this.titleId || this.id + '-title'
+      }
+    },
+    data: () => ({
+      dialog: null
+    }),
+    methods: {
+      close() {
+        this.dialog.hide()
+      }
+    },
+    mounted() {
+      this.dialog = new A11yDialog(this.$refs.rootElement, this.appRoot)
+      console.log('mounted', this.dialog)
+      this.$emit('dialog-ref', this.dialog)
+    },
+    destroyed() {
+      if (this.dialog) {
+        this.dialog.destroy()
+      }
+
+      this.$emit('dialog-ref')
+    }
+  }
+</script>
